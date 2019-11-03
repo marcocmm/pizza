21af1e1fef0d217c47798e0b98c95bfdbebb8ca4
marcocmm <marco.cmm@gmail.com>
2019-11-03 17:56:04 -0300

    Atributos da pizza com model e getall

---
+++ src/main/java/com/example/accessingdatamysql/Extra.java
@@ -0,0 +1,58 @@
+package com.example.accessingdatamysql;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author root
+ */
+@Entity // This tells Hibernate to make a table out of this class
+public class Extra implements Serializable  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id_extra;
+
+    private String descricao;
+
+    private Integer tempo;
+
+    private Integer valor;
+
+    public Integer getId_extra() {
+        return id_extra;
+    }
+
+    public void setId_extra(Integer id_extra) {
+        this.id_extra = id_extra;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getTempo() {
+        return tempo;
+    }
+
+    public void setTempo(Integer tempo) {
+        this.tempo = tempo;
+    }
+
+    public Integer getValor() {
+        return valor;
+    }
+
+    public void setValor(Integer valor) {
+        this.valor = valor;
+    }
+
+}
---
+++ src/main/java/com/example/accessingdatamysql/SaborRepository.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.accessingdatamysql;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author root
+ */
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+public interface SaborRepository extends CrudRepository<Sabor, Integer> {
+
+}
